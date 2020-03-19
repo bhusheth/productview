d2d6de7116a13576d478912f677b2ab10a75f788
Bhumit <bhusheth@gmail.com>
2020-03-18 23:57:41 -0400

    [TECH] Added axios for api call

--- src/sagas/productSaga.js
+++ src/sagas/productSaga.js
@@ -1,11 +1,13 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
+import Axios from 'axios';
 import { setAllProducts } from '../actions/productAction';
 import { GET_ALL_PRODUCT } from '../constants/productConstants';
 
 export default function* getAllProducts() {
 
-    const response = yield call(fetch, 'http://api.jsoneditoronline.org/v1/docs/b48e27bd6f55413eaf796c70dc3e96eb/data');
-    const responseBody = yield response.json();
+    let responseBody;
+    responseBody = yield Axios.get('http://api.jsoneditoronline.org/v1/docs/b48e27bd6f55413eaf796c70dc3e96eb/data')
+        .then(res => res.data)
     
     yield put(setAllProducts(responseBody.products));
 }
