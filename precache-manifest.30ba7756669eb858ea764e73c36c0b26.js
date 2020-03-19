6dd4711473f92c1f3227bc0e6354f84412ea2398
Bhumit <bhusheth@gmail.com>
2020-03-19 14:18:25 -0400

    Updates

---
+++ precache-manifest.30ba7756669eb858ea764e73c36c0b26.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba94ef681922b01018429cee3bc6e6c",
+    "url": "/bhusheth/productview/index.html"
+  },
+  {
+    "revision": "91a88c0307b3c61170e9",
+    "url": "/bhusheth/productview/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d2c4dcccd218105f9b60",
+    "url": "/bhusheth/productview/static/css/main.6aa0c03e.chunk.css"
+  },
+  {
+    "revision": "91a88c0307b3c61170e9",
+    "url": "/bhusheth/productview/static/js/2.6c95145e.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/bhusheth/productview/static/js/2.6c95145e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c4dcccd218105f9b60",
+    "url": "/bhusheth/productview/static/js/main.0a9bcb4b.chunk.js"
+  },
+  {
+    "revision": "d4d32a9a21ed2e9e6ec8",
+    "url": "/bhusheth/productview/static/js/runtime-main.5040494c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bhusheth/productview/static/media/logo.5d5d9eef.svg"
+  }
+]);
